d1573b4a5f14f2e02f04faf4eda1f153240b8d5b
Zeina Wady <zeinawady@gmail.com>
2025-03-03 00:20:24 +0200

    caesar implementation

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -10,18 +10,67 @@ namespace SecurityLibrary
     {
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
-            
+            //throw new NotImplementedException();
+            string encryptedText = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                if (plainText[i] >= 97 && plainText[i] <= 122)
+                {
+                    encryptedText +=(char)((plainText[i] -'a' + key + 26)%26 +'a');
+                }
+                else if (plainText[i] >= 65 && plainText[i] <= 90)
+                {
+                    encryptedText += (char)((plainText[i] -'A' + key + 26) % 26 +'A');
+                }
+                else
+                {
+                    encryptedText += plainText[i];
+                }
+            }
+
+            return encryptedText;
+
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+
+            //throw new NotImplementedException();
+           string plainText = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                if (cipherText[i] >= 97 && cipherText[i] <= 122)
+                {
+                    plainText += (char)((cipherText[i] - 'a' - key + 26) % 26 + 'a');
+                }
+                else if (cipherText[i] >= 65 && cipherText[i] <= 90)
+                {
+                    plainText += (char)((cipherText[i] - 'A' - key + 26) % 26 + 'A');
+                }
+                else
+                {
+                    plainText += cipherText[i];
+                }
+            }
+
+            return plainText;
+
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //find the key that is used in encryption
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+
+            int key =  cipherText[0] - plainText[0];
+            if (key < 0)
+            {
+            key += 26;
+            }
+            return key;
+
         }
     }
 }
