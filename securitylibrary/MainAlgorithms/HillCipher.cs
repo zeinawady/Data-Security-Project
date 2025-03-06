b21f4323c7a651f4be9859d7c7848d93dd8dc07c
KenzyAdel <kenzyadel16@gmail.com>
2025-03-06 05:25:54 +0200

    Added the Get inverse of 3*3-matrices function for hill cipher

--- securitylibrary/MainAlgorithms/HillCipher.cs
+++ securitylibrary/MainAlgorithms/HillCipher.cs
@@ -101,6 +101,87 @@ namespace SecurityLibrary
         }
 
 
+        public List<int> Inverse3by3(List<int> matrix, int bvalue)
+        {
+            /* k = [A   B   C]      =>      cofactor(K)= [+(EI−FH)  −(DI−FG)    +(DH−EG)]       => Transpose the cofactor matrix
+             *     [D   E   F]                           [−(BI−CH)  +(AI−CG)    −(AH−BG)]
+             *     [G   H   I]                           [+(BF−CE)  −(AF−CD)    +(AE−BD)]
+             *     
+             * Loop on the matrix and handle the negative values
+             * Get the modulo 26 of the matrix
+             * Multiply the matrix by the bValue then get the modulo 26 again
+             * det(K)=A(EI−FH)−B(DI−FG)+C(DH−EG)
+             */
+
+            if (matrix.Count != 9)
+            {
+                throw new Exception("Wrong matrix size. Must be 3*3");
+            }
+
+            int adjA, adjB, adjC, adjD, adjE, adjF, adjG, adjH, adjI;
+
+            adjA = (matrix[4] * matrix[8]) - (matrix[5] * matrix[7]);                 //+(EI−FH)
+
+            adjB = (matrix[3] * matrix[8]) - (matrix[5] * matrix[6]); adjB *= -1;     //−(DI−FG)
+
+            adjC = (matrix[3] * matrix[7]) - (matrix[4] * matrix[6]);                 //+(DH−EG)
+
+            adjD = (matrix[1] * matrix[8]) - (matrix[2] * matrix[7]); adjD *= -1;     //−(BI−CH)
+
+            adjE = (matrix[0] * matrix[8]) - (matrix[2] * matrix[6]);                 //+(AI−CG)
+
+            adjF = (matrix[0] * matrix[7]) - (matrix[1] * matrix[6]); adjF *= -1;     //−(AH−BG)
+
+            adjG = (matrix[1] * matrix[5]) - (matrix[2] * matrix[4]);                 //+(BF−CE)
+
+            adjH = (matrix[0] * matrix[5]) - (matrix[2] * matrix[3]); adjH *= -1;     //−(AF−CD)
+
+            adjI = (matrix[0] * matrix[4]) - (matrix[1] * matrix[3]);                 //+(AE−BD)
+
+            //Adding the ajoints in a transposed manner to the list
+            List<int> inverse3 = new List<int> { adjA, adjD, adjG, adjB, adjE, adjH, adjC, adjF, adjI };
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                while (inverse3[i] < 0)
+                {
+                    inverse3[i] += 26;
+                }
+                inverse3[i] %= 26;
+                inverse3[i] = (inverse3[i] * bvalue) % 26;    // Apply modular inverse multiplier
+            }
+            return inverse3;
+        }
+        public List<int> Multiply2by2(List<int> matrix1, List<int> matrix2)
+        {
+            if (matrix1.Count() != 4 || matrix2.Count() != 4)
+            {
+                throw new Exception("Wrong matrices size to be multiplied. Size must be 2*2");
+            }
+
+            int A, B, C, D;
+            A = (matrix1[0] * matrix2[0]) + (matrix1[1] * matrix2[2]);
+            B = (matrix1[0] * matrix2[1]) + (matrix1[1] * matrix2[3]);
+
+            C = (matrix1[2] * matrix2[0]) + (matrix1[3] * matrix2[2]);
+            D = (matrix1[2] * matrix2[1]) + (matrix1[3] * matrix2[3]);
+
+            List<int> multiply2by2 = new List<int> { A, B, C, D };
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (multiply2by2[i] < 0)
+                {
+                    multiply2by2[i] += 26;
+                }
+                multiply2by2[i] %= 26;
+            }
+
+            return multiply2by2;
+        }
+
+
         public List<int> Analyse(List<int> plainText, List<int> cipherText)
         {
             throw new NotImplementedException();
